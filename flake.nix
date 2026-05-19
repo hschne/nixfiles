9d68bc00576d89233331761bf7f34dee4bf15de7
hschne <hello@hansschnedlitz.com>
2026-05-19 18:23:19 +0200

    Switch nixpkgs to nixos-unstable for latest packages

--- flake.nix
+++ flake.nix
@@ -2,14 +2,27 @@
   description = "Hans's NixOS hosts";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... }: {
+  outputs = inputs@{ nixpkgs, agenix, ... }: {
+    apps.x86_64-linux.agenix = {
+      type = "app";
+      program = "${agenix.packages.x86_64-linux.default}/bin/agenix";
+    };
+
+    packages.x86_64-linux.agenix = agenix.packages.x86_64-linux.default;
+
     nixosConfigurations = {
       anubis = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
         modules = [
+          agenix.nixosModules.default
           ./hosts/anubis
         ];
       };
